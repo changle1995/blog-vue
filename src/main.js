d99693fc4f261319a5a16d1fc80a8a8eeeea2557
changle <changle@xmylsw.com>
2018-06-29 16:55:30 +0800

    使用mavon-editor插件

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App'
 import router from './config/router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import MavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 import store from './vuex/store'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(MavonEditor)
 
 new Vue({
   router,
