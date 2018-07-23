af0a84ab104798abe13fe2fa20f0b6e64288e153
changle <changle@xmylsw.com>
2018-07-23 23:22:01 +0800

    博客页脚完全确定

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,32 +2,78 @@
   <footer class="container-fluid blog-footer pt-5 border-top">
     <div class="row">
       <div class="col">
-        <h5>Features</h5>
+        <h5>后端技术</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="https://www.baidu.com">Cool stuff</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Random feature</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Team feature</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Stuff for developers</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Another one</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Last time</a></li>
+          <li>
+            <a class="text-muted"
+               href="https://spring.io/projects/spring-framework/"
+               target="_blank">spring-framework
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="https://spring.io/projects/spring-boot/"
+               target="_blank">spring-boot
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="https://spring.io/projects/spring-security/"
+               target="_blank">spring-security
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="http://projects.spring.io/spring-data-jpa/"
+               target="_blank">spring-data-jpa
+            </a>
+          </li>
         </ul>
       </div>
       <div class="col">
-        <h5>Resources</h5>
+        <h5>前端技术</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="https://www.baidu.com">Resource</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Resource name</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Another resource</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Final resource</a></li>
+          <li>
+            <a class="text-muted"
+               href="https://cn.vuejs.org/"
+               target="_blank">Vue
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="https://router.vuejs.org/"
+               target="_blank">Vue Router
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="https://vuex.vuejs.org/"
+               target="_blank">Vuex
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="http://www.bootcss.com/"
+               target="_blank">Bootstrap
+            </a>
+          </li>
         </ul>
       </div>
       <div class="col">
-        <h5>About</h5>
+        <h5>项目源码</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" href="https://www.baidu.com">Team</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Locations</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Privacy</a></li>
-          <li><a class="text-muted" href="https://www.baidu.com">Terms</a></li>
+          <li>
+            <a class="text-muted"
+               href="https://github.com/changle1995/blog-vue/"
+               target="_blank">前端项目
+            </a>
+          </li>
+          <li>
+            <a class="text-muted"
+               href="https://github.com/changle1995/blog/"
+               target="_blank">后端项目
+            </a>
+          </li>
         </ul>
       </div>
     </div>
